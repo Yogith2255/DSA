fc0e084014c8bc0461730aafafb65bfdaae20f71
Yogith2255 <kattayogithreddy@gmail.com>
2026-09-23 07:54:10 +0530

    Time: 7 ms (91.82%) | Memory: 22.9 MB (40.39%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.py
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.py
@@ -3,20 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from math import gcd
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
         temp=head
         while temp and temp.next:
             a=temp.val
             b=temp.next.val
-            for i in range(min(a,b),0,-1):
-                if a%i==0 and b%i==0:
-                    newNode=ListNode(i)
-                    nextnode=temp.next
-                    temp.next=newNode
-                    newNode.next=nextnode
-                    break
-            temp=newNode.next
+            g = gcd(temp.val, temp.next.val)
+            newNode = ListNode(g)
+            nextnode = temp.next
+            temp.next = newNode
+            newNode.next = nextnode
+
+            temp = nextnode
+            
         return head
 
         
